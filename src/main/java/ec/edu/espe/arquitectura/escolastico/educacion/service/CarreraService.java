4167a85b22a7cf3480b7ba1fea31df9a5c79ddbb
Wilmer <wsalquinga@espe.edu.ec>
2022-06-11 03:59:55 -0500

    Agregando servicios para MallaCarrera (en CarreraService)

--- src/main/java/ec/edu/espe/arquitectura/escolastico/educacion/service/CarreraService.java
+++ src/main/java/ec/edu/espe/arquitectura/escolastico/educacion/service/CarreraService.java
@@ -5,22 +5,36 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import ec.edu.espe.arquitectura.escolastico.educacion.MallaCarreraException;
 import ec.edu.espe.arquitectura.escolastico.educacion.dao.CarreraRepository;
+import ec.edu.espe.arquitectura.escolastico.educacion.dao.MallaCarreraRepository;
 import ec.edu.espe.arquitectura.escolastico.educacion.model.Carrera;
+import ec.edu.espe.arquitectura.escolastico.educacion.model.MallaCarrera;
 
 @Service
 public class CarreraService {
 
     private CarreraRepository carreraRepository;
+    private MallaCarreraRepository mallaCarreraRepository;
 
-    public CarreraService(CarreraRepository carreraRepository) {
+    public CarreraService(CarreraRepository carreraRepository, MallaCarreraRepository mallaCarreraRepository) {
         this.carreraRepository = carreraRepository;
+        this.mallaCarreraRepository = mallaCarreraRepository;
     }
 
     public Carrera obtenerPorCodigo(Integer codigo) {
         Optional<Carrera> carreraOpt = this.carreraRepository.findById(codigo);
         if(carreraOpt.isPresent()) {
             return carreraOpt.get();
+        } else {
+            return null;
+        }
+    }
+
+    public MallaCarrera obtenerMallaCarreraPorCodigo(Integer codigo) {
+        Optional<MallaCarrera> mallaCarreraOpt = this.mallaCarreraRepository.findById(codigo);
+        if(mallaCarreraOpt.isPresent()) {
+            return mallaCarreraOpt.get();
         } else {
             return null;
         }
@@ -55,6 +69,38 @@ public class CarreraService {
 
     public List<Carrera> listarCarrerasPorNombre(String nombrePattern) {
         return this.carreraRepository.findByNombreLikeOrderByNombreAsc(nombrePattern);
+    }
+
+    public List<MallaCarrera> obtenerMalla(Integer codigo) throws MallaCarreraException {
+        Carrera carrera = this.obtenerPorCodigo(codigo);
+        if(carrera == null) {
+            throw new MallaCarreraException("No existe la Carrera para el código recibido.");
+        }
+        List<MallaCarrera> materiasMalla = this.mallaCarreraRepository.findByCodCarreraOrderByNivelAsc(codigo);
+        if(materiasMalla.isEmpty()) {
+            throw new MallaCarreraException("No existen materias en la malla de esta Carrera");
+        } else {
+            return materiasMalla;
+        }
+    }
+
+    public List<MallaCarrera> obtenerMateriasPorNivel(Integer codCarrera, Integer nivel) throws MallaCarreraException {
+        Carrera carrera = this.obtenerPorCodigo(codCarrera);
+        if(carrera == null) {
+            throw new MallaCarreraException("No existe la Carrera para el código recibido.");
+        }
+        List<MallaCarrera> materiasMalla = this.mallaCarreraRepository.findByCodCarreraAndNivel(codCarrera, nivel);
+        if(materiasMalla.isEmpty()) {
+            throw new MallaCarreraException("No existen materias en en el Nivel recibido.");
+        } else {
+            return materiasMalla;
+        }
+    }
+
+    public void modificarMateriMallaCarrera(MallaCarrera materiaCarrera) {
+        MallaCarrera materiaCarreraBD = this.obtenerMallaCarreraPorCodigo(materiaCarrera.getCodMateriaCarrera());
+        materiaCarreraBD.setNivel(materiaCarrera.getNivel());
+        this.mallaCarreraRepository.save(materiaCarreraBD);
     }
     
 }
