40d5be98328fa46b647d7db0318fac6cef5c0fba
Wilmer <wsalquinga@espe.edu.ec>
2022-06-13 18:22:16 -0500

    Borrando imports innecesarios en NrcService

--- src/main/java/ec/edu/espe/arquitectura/escolastico/educacion/service/NrcService.java
+++ src/main/java/ec/edu/espe/arquitectura/escolastico/educacion/service/NrcService.java
@@ -3,38 +3,21 @@ package ec.edu.espe.arquitectura.escolastico.educacion.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.stereotype.Service;
 
-import ec.edu.espe.arquitectura.escolastico.educacion.dao.MateriaRepository;
 import ec.edu.espe.arquitectura.escolastico.educacion.dao.NrcHorarioRepository;
 import ec.edu.espe.arquitectura.escolastico.educacion.dao.NrcRepository;
-import ec.edu.espe.arquitectura.escolastico.educacion.dao.PeriodoRepository;
-import ec.edu.espe.arquitectura.escolastico.educacion.model.Materia;
-import ec.edu.espe.arquitectura.escolastico.educacion.model.MateriaPK;
 import ec.edu.espe.arquitectura.escolastico.educacion.model.Nrc;
 import ec.edu.espe.arquitectura.escolastico.educacion.model.NrcPK;
-import ec.edu.espe.arquitectura.escolastico.educacion.model.Periodo;
-import ec.edu.espe.arquitectura.escolastico.persona.dao.PersonaRepository;
-import ec.edu.espe.arquitectura.escolastico.persona.model.Persona;
 
 @Service
 public class NrcService {
 
     private NrcRepository nrcRepository;
-    private MateriaRepository materiaRepository;
-    private PeriodoRepository periodoRepository;
-    private PersonaRepository personaRepository;
     private NrcHorarioRepository nrcHorarioRepository;
 
-    public NrcService(NrcRepository nrcRepository, MateriaRepository materiaRepository,
-            PeriodoRepository periodoRepository, PersonaRepository personaRepository,
-            NrcHorarioRepository nrcHorarioRepository) {
+    public NrcService(NrcRepository nrcRepository, NrcHorarioRepository nrcHorarioRepository) {
         this.nrcRepository = nrcRepository;
-        this.materiaRepository = materiaRepository;
-        this.periodoRepository = periodoRepository;
-        this.personaRepository = personaRepository;
         this.nrcHorarioRepository = nrcHorarioRepository;
     }
 
